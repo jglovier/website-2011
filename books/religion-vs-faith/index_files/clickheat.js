3f5b92459f92022390a3570a0cde2515772524be
Joel Glovier <jglovier@github.com>
2016-05-14 10:46:51 -0400

    initial dump

---
+++ books/religion-vs-faith/index_files/clickheat.js
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en-us" class="no-js">
+
+<head>
+  <meta charset="UTF-8">
+
+  <!--
+        $$$$$$$$$$  $$$$$$$$$$  $$$$$$$$$$
+                $$  $$      $$  $$
+        $$      $$  $$$$$$$$$$  $$      $$
+        $$$$$$$$$$  $$      $$  $$$$$$$$$$
+
+               ____==========_______
+    _--____   |    | ""  " "|       \
+   /  )8}  ^^^| 0  |  =     |  o  0  |
+ </_ +-==B vvv|""  |  =     | '  "" "|
+    \_____/   |____|________|________|
+             (_(  )\________/___(  )__)
+               |\  \            /  /\
+               | \  \          /  /\ \
+               | |\  \        /  /  \ \
+               (  )(  )       (  \   (  )
+                \  / /        \  \   \  \
+                 \|  |\        \  \  |  |
+                  |  | )____    \  \ \  )___
+                  (  )  /  /    (  )  (/  /
+                 /___\ /__/     /___\ /__/
+                The AT-AT, By Core21
+                (ASCII Art from: http://www.chris.com/ascii/index.php?art=movies%2Fstar%20wars)
+-->
+  <title>joelglovier.com</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Lora:400,700|Open+Sans:400,300,600,800,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="/css/main.css" />
+  
+  <link rel="icon" type="image/x-icon" href="/favicon.png?v=3" />
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.png?v=3" />
+  <link rel="canonical" href="http://joelglovier.com/404.html" />
+  <link rel="author" href="https://plus.google.com/+JoelGlovier/posts">
+  <link href='http://joelglovier.com/feed.xml' rel='alternate' type='application/rss+xml'>
+  <meta name="copyright" content="Joel Andrew Glovier 2013" />
+  <meta name="description" content="The personal site of Joel Andrew Glovier, designer and front-end technologist. Where I write about code, design, faith, food, and life. Views expressed are my own. Use code at your own risk." />
+  <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="google-site-verification" content="rfhW_LUYYbZ4kwHGGZqplNsGs5eFB1-H8UEfmF6lE9o" />
+
+  
+
+  
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="http://joelglovier.com/img/ogimgs/default.jpg" />
+  <meta name="twitter:card" content="summary">
+  
+
+  
+
+  
+
+  <meta name="twitter:domain" content="joelglovier.com">
+  <meta name="twitter:site" content="@jglovier">
+  <meta name="twitter:creator" content="@jglovier">
+  <meta property="og:site_name" content="Joel Glovier's blog" />
+  <meta property="og:url" content="http://joelglovier.com/404.html/" />
+  <!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="/js/site.js"></script>
+</head>
+
+
+<body class="page404">
+
+  <div class="bg-img"></div>
+
+  <div class="page-content">
+
+      <nav class="site-nav">
+
+    <a href="#" class="nav-trigger">
+      <svg class="menu-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="21">
+        <path fill-rule="evenodd" clip-rule="evenodd" fill="#333333" d="M2.154,16.8h23.692C27.036,16.8,28,17.74,28,18.9
+          c0,1.16-0.964,2.1-2.154,2.1H2.154C0.964,21,0,20.06,0,18.9C0,17.74,0.964,16.8,2.154,16.8z M2.154,8.4h23.692
+          C27.036,8.4,28,9.34,28,10.5c0,1.16-0.964,2.1-2.154,2.1H2.154C0.964,12.6,0,11.66,0,10.5C0,9.34,0.964,8.4,2.154,8.4z M2.154,0
+          h23.692C27.036,0,28,0.94,28,2.1c0,1.16-0.964,2.1-2.154,2.1H2.154C0.964,4.2,0,3.26,0,2.1C0,0.94,0.964,0,2.154,0z"/>
+        </svg>
+    </a>
+
+    <ul class="links">
+      <li><a href="/">Home</a></li>
+      <li><a href="/about/">About</a></li>
+      <li><a href="/#writing">Writing</a></li>
+      <li><a href="/speaking/">Speaking</a></li>
+      <!-- <li><a href="/projects">Projects</a></li>
+      <li><a href="/#elsewhere">Elsewhere</a></li> -->
+    </ul>
+
+  </nav>
+
+    <header>
+
+      <div class="wrap">
+
+        <div class="intro-copy">
+          <h1><a href="/">404</a></h1>
+          <p class="subhead">Page not found</p>
+        </div>
+
+        <p class="intro-right">The sun has set on the page you were in search of.</p>
+
+      </div>
+
+    </header>
+
+    <div class="content">
+
+      <div class="wrap">
+
+    <h2 id="what-does-this-even-mean">What does this even mean??</h2>
+
+<p>This is a “404 error” page. What it means is the page you were looking for has moved - either the URL structure changed or the page simply no longer exists on this site and the link you followed to get here - whether from Google or someone else’s website, is out of date. I’m really sorry about that!</p>
+
+<h2 id="choose-your-adventure">Choose your adventure:</h2>
+
+<ol>
+  <li>Go to the <a href="http://joelglovier.com">homepage (joelglovier.com)</a> and try to find what you are looking for from there.</li>
+  <li>Try adding “2011.joelglovier.com” before the url you tried, as in “joelglovier.com/some-old-path” may now live at “2011.joelglovier.com/some-old-path”. (This is because I recently restructured the site and broke some older links, but they are still available under the “2011” subdomain.)</li>
+  <li><a href="http://twitter.com/home?status=@jglovier%20Yo%20Joel%20I%20404%27d%20on%20your%20site%20homie%2E%20You%20should%20really%20fix%20that%20shiz%2E%20The%20page%20I%20was%20looking%20for%20is%3A">Hollar at me on Twitter</a>. Seriously. Hollar away.</li>
+</ol>
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<!--GA script here-->
+<script>
+
+  var _gaq = _gaq || [];
+  var pluginUrl =
+   '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+  _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+  _gaq.push(['_setAccount', 'UA-17567335-1']);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_trackPageLoadTime']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+
+</html>
